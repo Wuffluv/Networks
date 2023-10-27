63917d5e4cba6db0fd9b74b3cb8aa9b71cdb073f
Wuffluv <rodion.rycha@yandex.ru>
2023-10-28 00:31:10 +0900

    complete lab

--- Манчестерское кодирование/Manchester.py
+++ Манчестерское кодирование/Manchester.py
@@ -1,14 +1,26 @@
-import matplotlib
+# Функция для кодирования байта по Манчестерскому коду
+def manchester_encode_byte(byte):
+    encoded = []
+    for i in range(7, -1, -1):
+        # Кодирование данных
+        if (byte >> i) & 1:
+            # Если текущий бит равен 1, то добавляем переход от низкого к высокому уровню
+            encoded.extend([True, False])
+        else:
+            # Если текущий бит равен 0, то добавляем переход от высокого к низкому уровню
+            encoded.extend([False, True])
+    return encoded
 
-def manchester(low, high):
-    y = []
-    for i in range(8):
-        if raw_data[i] == 0:
-            y.extend([low, low, high, high])
-            plt.annotate("", xy=(i + 0.5, low + 0.51), xytext=(i + 0.5, low + 0.49),
-                         arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
-        elif raw_data[i] == 1:
-            y.extend([high, high, low, low])
-            plt.annotate("", xy=(i + 0.5, low + 0.49), xytext=(i + 0.5, low + 0.51),
-                         arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
-    plt.plot(x_standard, y)
+# Фраза, которую мы хотим закодировать
+message = "hello world"
+
+# Кодирование фразы
+encoded_message = []
+for c in message:
+    encoded_byte = manchester_encode_byte(ord(c))
+    encoded_message.extend(encoded_byte)
+
+# Вывод закодированной последовательности в консоли
+for bit in encoded_message:
+    print("01" if bit else "10", end="")
+print()
