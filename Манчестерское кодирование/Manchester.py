27d96d84b7de3e1c18009f9baa6185ae24221743
Wuffluv <rodion.rycha@yandex.ru>
2023-10-28 14:26:20 +0900

    Добавил графический вывод

--- Манчестерское кодирование/Manchester.py
+++ Манчестерское кодирование/Manchester.py
@@ -1,6 +1,11 @@
+import matplotlib.pyplot as plt
+
 # Функция для кодирования байта по Манчестерскому коду
 def manchester_encode_byte(byte):
     encoded = []
+    timestamps = []  # Список для хранения временных меток
+    t = 0  # Инициализация временной метки
+
     for i in range(7, -1, -1):
         # Кодирование данных
         if (byte >> i) & 1:
@@ -9,18 +14,37 @@ def manchester_encode_byte(byte):
         else:
             # Если текущий бит равен 0, то добавляем переход от высокого к низкому уровню
             encoded.extend([False, True])
-    return encoded
+
+        # Обновляем временную метку для каждого бита
+        timestamps.extend([t, t + 0.5])
+        t += 1  # Увеличиваем временную метку
+
+    return encoded, timestamps
 
 # Фраза, которую мы хотим закодировать
 message = "hello world"
 
 # Кодирование фразы
 encoded_message = []
+timestamps_message = []
+t = 0  # Инициализация временной метки
+
 for c in message:
-    encoded_byte = manchester_encode_byte(ord(c))
+    encoded_byte, timestamps_byte = manchester_encode_byte(ord(c))
     encoded_message.extend(encoded_byte)
+    timestamps_message.extend([t + t_byte for t_byte in timestamps_byte])
+    t += len(encoded_byte)  # Обновляем временную метку
 
 # Вывод закодированной последовательности в консоли
 for bit in encoded_message:
     print("01" if bit else "10", end="")
 print()
+
+# Построение графика
+plt.figure(figsize=(10, 2))
+plt.step(timestamps_message, [int(bit) for bit in encoded_message], where='post', color='b')
+plt.xlabel('Время')
+plt.ylabel('Сигнал (0/1)')
+plt.title('Manchester Encoding')
+plt.grid(True)
+plt.show()
